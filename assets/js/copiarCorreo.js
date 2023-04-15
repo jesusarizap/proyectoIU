27d68a4916362311f886f39d2c6658430e34d73c
PedroAnkersmit <pedrankersmit99@gmail.com>
2023-04-15 20:27:49 +0200

    Añadida funcionalidad al boton copiar correo electronico

--- assets/js/copiarCorreo.js
+++ assets/js/copiarCorreo.js
@@ -1,6 +1,12 @@
 function copiarCorreo() {
-    var copyText = document.getElementById("correo");
+    var copyText = "tournamastercontact@gmail.com";
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
+    alert("Copiado al portapapeles");
+}
+
+function outFunc(){
+    var tooltip = document.getElementById("copiarCorreo");
+    tooltip.innerHTML = "Copiar al portapapeles";
 }
