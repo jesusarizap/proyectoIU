874c703d5755f4918c18a8bf5e12633b728015d5
LAPTOP-UOADHCKT\Pedro <pedroankersmituni@gmail.com>
2023-04-18 20:10:08 +0200

    Barra de Búsqueda no se superpone a la sidebar

--- assets/js/sidebarCollapse.js
+++ assets/js/sidebarCollapse.js
@@ -1,8 +1,19 @@
 const expandBtn = document.querySelector('.expand-btn');
 const sidebar = document.querySelector('.sidebar');
+const barraB = document.getElementById("barraBusqueda");
 
 expandBtn.addEventListener('click', function() {
   sidebar.classList.toggle('expanded');
   const isExpanded = sidebar.classList.contains('expanded');
   expandBtn.setAttribute('aria-expanded', isExpanded);
 });
+
+
+
+barraB.addEventListener('click', function(){
+  if(sidebar.classList.contains('expanded')){
+  sidebar.classList.toggle('expanded');
+  const isExpanded = sidebar.classList.contains('expanded');
+  expandBtn.setAttribute('aria-expanded', isExpanded);
+}
+})
